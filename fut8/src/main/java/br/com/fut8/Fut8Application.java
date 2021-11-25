a19f847fb01ef3600ca16134a1bbde228a9114f2
junioryes13 <leojunior1326@gmail.com>
2021-11-24 22:40:10 -0300

    Correção de alguns endpoints

---
+++ fut8/src/main/java/br/com/fut8/Fut8Application.java
@@ -0,0 +1,14 @@
+package br.com.fut8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+public class Fut8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fut8Application.class, args);
+	}
+
+}
---
+++ fut8/src/test/java/br/com/fut8/Fut8ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fut8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fut8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
